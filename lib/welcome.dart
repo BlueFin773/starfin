6a5254b5e7e3074c549b0d223af0ef0350c3d79d
Matthew Litvinov <bluefin@Matthews-MacBook-Air.local>
2020-12-01 11:50:16 -0500

    style changes on welcome page

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -13,8 +13,23 @@ class _WelcomePageState extends State<WelcomePage> {
         padding: const EdgeInsets.all(32),
         child: Column(
           children:[
-            Text("Welcome to StarFin!"),
-            Text("StarFin helps you create a custom day plan to visit tourist attractions in the Toronto area based on your preference."),
+            SizedBox(
+              height: 40,
+            ),
+            Text("Welcome to StarFin!",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36, ),
+              textAlign: TextAlign.center
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text("StarFin helps you create a custom day plan to visit tourist attractions in the Toronto area based on your preferences.",
+                style: TextStyle(fontSize:18),
+                textAlign: TextAlign.center
+            ),
+            SizedBox(
+              height: 80,
+            ),
             Image.asset('assets/images/map.png'),
           ],
         ),
