532d2f7d641bf4bdabe78efa95455e69f6ae2864
Matthew Litvinov <matthew.litvinov@gmail.com>
2020-12-07 12:57:38 -0500

    added sliders to rate it page

--- lib/rate.dart
+++ lib/rate.dart
@@ -6,6 +6,11 @@ class RatePage extends StatefulWidget {
 }
 
 class _RatePageState extends State<RatePage> {
+  int _culture = 6;
+  int _nature = 4;
+  int _entertainment = 7;
+  int _historical = 3;
+  int _architecture = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +34,123 @@ class _RatePageState extends State<RatePage> {
                 textAlign: TextAlign.center
             ),
 
-            //TODO: Create sliders
+            SizedBox(
+              height: 40,
+            ),
+
+            // culture slider
+            Text(
+                  "Culture",
+                  style: TextStyle(fontSize: 18),
+                  textAlign: TextAlign.center
+            ),
+              new Expanded(
+              child: Slider(
+                value: _culture.toDouble(),
+                min:1.0,
+                max:10.0,
+                divisions:9,
+                activeColor: Color(0xFF6C63FF),
+                inactiveColor: Color(0xFF707070),
+                onChanged:(double newValue){
+                  setState((){
+                    _culture = newValue.round();
+                  });
+                },
+              ),
+            ),
+
+            // nature slider
+            Text(
+                "Nature",
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center
+            ),
+            new Expanded(
+              child: Slider(
+                value: _nature.toDouble(),
+                min:1.0,
+                max:10.0,
+                divisions:9,
+                activeColor: Color(0xFF6C63FF),
+                inactiveColor: Color(0xFF707070),
+                onChanged:(double newValue){
+                  setState((){
+                    _nature = newValue.round();
+                  });
+                },
+              ),
+            ),
+
+            // entertainment slider
+            Text(
+                "Entertainment",
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center
+            ),
+            new Expanded(
+              child: Slider(
+                value: _entertainment.toDouble(),
+                min:1.0,
+                max:10.0,
+                divisions:9,
+                activeColor: Color(0xFF6C63FF),
+                inactiveColor: Color(0xFF707070),
+                onChanged:(double newValue){
+                  setState((){
+                    _entertainment = newValue.round();
+                  });
+                },
+              ),
+            ),
+
+            // historical slider
+            Text(
+                "Historical",
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center
+            ),
+            new Expanded(
+              child: Slider(
+                value: _historical.toDouble(),
+                min:1.0,
+                max:10.0,
+                divisions:9,
+                activeColor: Color(0xFF6C63FF),
+                inactiveColor: Color(0xFF707070),
+                onChanged:(double newValue){
+                  setState((){
+                    _historical = newValue.round();
+                  });
+                },
+              ),
+            ),
+
+            // architecture slider
+            Text(
+                "Architecture",
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center
+            ),
+            new Expanded(
+              child: Slider(
+                value: _architecture.toDouble(),
+                min:1.0,
+                max:10.0,
+                divisions:9,
+                activeColor: Color(0xFF6C63FF),
+                inactiveColor: Color(0xFF707070),
+                onChanged:(double newValue){
+                  setState((){
+                    _architecture = newValue.round();
+                  });
+                },
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+
 
             RaisedButton(
               onPressed: () {},
